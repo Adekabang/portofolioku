4f9e81793ea268ff2bd22d33a44843beb4e58b4e
Adekabang <iban.ninja@gmail.com>
2020-08-17 02:59:17 +0700

    fix script and create PortofolioList include modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import PortofolioList from "./components/PortofolioList";
 function App() {
   return (
     <div id="page-top">
       <Navbar />
       <Hero />
+      <PortofolioList />
     </div>
   );
 }
