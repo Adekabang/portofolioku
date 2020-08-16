8e49e3ba2626315ee1283cf1a37b46b26a7db081
Adekabang <iban.ninja@gmail.com>
2020-08-17 03:02:29 +0700

    create About component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import PortofolioList from "./components/PortofolioList";
+import About from "./components/About";
 function App() {
   return (
     <div id="page-top">
       <Navbar />
       <Hero />
       <PortofolioList />
+      <About />
     </div>
   );
 }
